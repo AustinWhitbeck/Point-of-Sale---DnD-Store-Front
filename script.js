3d32fe1f894ef978abfe3035466d4a23a8558f3f
Austin Whitbeck <austinwhitbeckart@gmail.com>
2021-05-19 19:30:13 -0400

    headers buttons working

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const imagesEquip = document.querySelector('.image-container-equip');
 const imagesSpells = document.querySelector('.image-container-spell');
 const imagesSkills = document.querySelector('.image-container-skill');
 const shopitems = document.querySelectorAll('.shop-item');
+const shopOptions = document.querySelector('.shop-options');
 const inventoryContainer = document.querySelector('.inventory-container');
 const inventoryContent = document.querySelector('.inventory-content');
 const headers = document.querySelector('.headers-div');
 const menuItems = document.querySelectorAll('.menu-item');
 const mainContent = document.querySelector('.main-content');
-console.log(mainContent);
+const cartContent = document.querySelector('.cart-content');
 
 
 
@@ -19,20 +20,29 @@ console.log(mainContent);
 headers.addEventListener('click',(event)=> {
         if (event.target.innerText === 'Shop'){
             mainContent.classList.remove('hidden');
+            shopOptions.classList.remove('hidden');
             inventoryContent.classList.add('hidden');
-            console.log(inventoryContainer);
-            
-        } else {
+            cartContent.classList.add('hidden');
+        } else if (event.target.innerText === 'Cart') {
+            console.log('cart');
+            mainContent.classList.add('hidden');
+            inventoryContent.classList.add('hidden');
+            shopOptions.classList.add('hidden');
+            cartContent.classList.remove('hidden');
+        } else if (event.target.innerText === 'Inventory') {
             console.log('inventory');
             mainContent.classList.add('hidden');
             inventoryContent.classList.remove('hidden');
-            console.log(mainContent);
-        }
+            shopOptions.classList.add('hidden');
+        } 
 })
+
 //  this can be condensed into one event listener for the nav bar.
 // this will look for the class of the nav li and then execute the below click
 // currently this is set up taht it would be an event listener for each of the nav buttons, however we can
 // condense this down into one event listener for a click on any of the nav buttons.
+
+
 allNavButton.addEventListener('click',(event)=> {
     shopitems.forEach((element) => {
         if (element.classList.contains('hidden')){
